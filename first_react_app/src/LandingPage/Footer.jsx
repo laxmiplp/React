d25dbefafad63698e5b66e2494d781d08f7ce3f6
laxmiplp <laxmiprasanna.plp@gmail.com>
2025-03-21 11:03:10 +0530

    task done

---
+++ first_react_app/src/LandingPage/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <nav class="navbar fixed-bottom bg-body-tertiary">
+            <div class="container-fluid text-center bg-dark m-auto w-100">
+                <a class="navbar-brand text-light" href="#">Footer</a>
+            </div>
+        </nav>
+    )
+}
+export default Footer
--- first_react_app/src/main.jsx
+++ first_react_app/src/main.jsx
@@ -1,9 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import Header from './LandingPage/Header.jsx'
+import Banner from './LandingPage/Banner.jsx'
+import Footer from './LandingPage/Footer.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Header />
+    <Banner />
+    <Footer />
   </StrictMode>,
 )
